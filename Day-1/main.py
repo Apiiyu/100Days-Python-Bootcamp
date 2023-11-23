6b54bec976282bbe186da87ee11616983386a241
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-11-23 15:56:31 +0700

    feat: practice built-in function and variable

---
+++ Day-1/main.py
@@ -0,0 +1,12 @@
+# Printing to the console
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# String Manipulation and Code Intelligence
+print("Hello World!\nHello World!\nHello World!") # \n is a new line
+print("Hello" + " " + "Apiiyu") # String concatenation
+
+# Variables
+name = input("What is your name? ")
+length = len(name) # len() is a function that returns the length of a string
---
+++ Day-1/project.py
@@ -0,0 +1,16 @@
+""" 
+  Project 1: Band Name Generator
+ 
+  Instructions:
+  Create a greeting for your program.
+  Ask the user for the city that they grew up in.
+  Ask the user for the name of a pet.
+  Combine the name of their city and pet and show them their band name.
+"""
+
+# Solution
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("\n\nYour band name could be " + city + " " + pet)
