7d0f8e9582e32f903bc9972a1547603b8f38371a
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-11-27 14:13:00 +0700

    feat: practice python loops

---
+++ Day-5/loops.py
@@ -0,0 +1,13 @@
+# Loops
+
+# For Loop
+# Syntax: for <variable> in <sequence>:
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits: # Please, use the meaningful variable name to make your code more readable.
+  print(fruit)
+ 
+total = 0 
+for number in range(1, 101): # range(start, end, step)
+  total += number
+
+print(total) # 5050
